3a67d8cd87173ddff74a7ab01b6415a04b762987
demetreus420 <dima.net.dd@gmail.com>
2021-02-17 17:16:34 +0200

    React app created

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyledHeader} from '../Styled/StyledHeader';
+
+const Header = () => {
+    return(
+        <StyledHeader>
+            Hello
+        </StyledHeader>
+    )
+}
+
+export {
+    Header
+}
---
+++ src/Components/Styled/StyledHeader.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.div`
+    margin: 0 auto;
+    width: 100%;
+    height: 100px;
+    background-color: orange;
+    color: white;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+
+export {
+    StyledHeader
+}
